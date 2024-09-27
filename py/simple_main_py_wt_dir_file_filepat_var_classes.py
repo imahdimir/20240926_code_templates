5304c9c2b2986965d7a0cf3fb30083c71b6e2cae
Mahdi <imahdimir@gmail.com>
2024-09-26 18:31:03 -0700

    + simple_main_py_wt_dir_file_filepat_var_classes.py

---
+++ py/simple_main_py_wt_dir_file_filepat_var_classes.py
@@ -0,0 +1,62 @@
+"""
+
+    """
+
+from pathlib import Path
+
+import pandas as pd
+
+
+class Directory :
+    proj_data = ''
+    proj_data = Path(proj_data)
+    p = proj_data
+
+    inp = p / 'inp'
+    med = p / 'med'
+    out = p / 'out'
+
+
+DIR = Directory()
+D = DIR
+
+
+class FilePath :
+    d = Directory()
+
+
+FILE_PATH = FilePath()
+FP = FILE_PATH
+
+
+class FilePathPattern :
+    d = Directory()
+
+
+FILE_PATH_PAT = FilePathPattern()
+FPT = FILE_PATH_PAT
+
+
+class Var :
+    pass
+
+
+VAR = Var()
+V = VAR
+
+
+def main() :
+    pass
+
+    ##
+
+
+    ##
+
+
+    ##
+
+
+##
+if __name__ == '__main__' :
+    main()
